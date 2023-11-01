9ccb4c183c6ca29982fb181ca897a1d64961719f
nikhilkaimalinsignia <130687289+nikhilkaimalinsignia@users.noreply.github.com>
2023-11-01 15:21:19 +0530

    initial commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,103 +1,26 @@
-//import "./App.css";
-import "./App.scss";
-import { init } from "commandbar";
-import { useNavigate, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-import reactRouterToArray from "react-router-to-array";
-import Home from "./pages/Home/";
-import getRoutes from "./routes";
-// import ReactGA from "react-ga";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// const TRACKING_ID = "UA-268238248-1";
-
-// ReactGA.initialize(TRACKING_ID);
-
-init("72888fac");
-
-function App({ children }: any) {
-  const navigate = useNavigate();
-  const [commandBarReady, setCommandBarReady] = useState(false);
-
-  const indexRoute = (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="*"
-        element={<div style={{ display: "none" }}>Not Found</div>}
-      />
-    </Routes>
-  );
-
-  const routes = getRoutes();
-
-  var routesArray = reactRouterToArray(routes);
-
-  routesArray = routesArray.map((route: any) => route.replace("/", ""));
-
-  // useEffect(() => {
-  //   ReactGA.pageview(window.location.pathname + window.location.search);
-  // }, []);
-
-  useEffect(() => {
-    window.CommandBar.boot("me").then(() => {
-      setCommandBarReady(true);
-    });
-
-    return window.CommandBar.shutdown;
-  }, []);
-
-  useEffect(() => {
-    if (commandBarReady) {
-      window.CommandBar.setFormFactor({
-        type: "inline",
-        rootElement: document.querySelector("#commandbar-inline-root"),
-      });
-
-      window.CommandBar.setCustomComponent(
-        "footer",
-        () =>
-          `<div className="p-2">
-          <i>
-            <small>Powered by InsigniaSearch</small>
-          </i>
-        </div>`
-      );
-
-      if (routesArray.length > 0) {
-        window.CommandBar.addRecords(
-          "Pages",
-          routesArray.map((route: any) => {
-            return { label: `${route}`, id: `${route}` };
-          })
-        );
-      }
-
-      window.CommandBar.addRecordAction("Pages", {
-        text: "Open Page",
-        name: "open_page",
-        template: {
-          type: "link",
-          value: "/{{record.id}}",
-          operation: "self",
-        },
-      });
-    }
-  }, [commandBarReady, routesArray]);
-
-  useEffect(() => {
-    if (commandBarReady) {
-      window.CommandBar.addRouter(navigate);
-    }
-  }, [navigate, commandBarReady]);
-
+function App() {
   return (
     <div className="App">
-      {children}
-      {indexRoute}
-      {routes}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-

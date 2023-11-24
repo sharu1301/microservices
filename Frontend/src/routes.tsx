3b782288054af1046f7e39b68f3c528e55e1d9aa
sonal <sonal@insigniasonsultancy.com>
2023-11-24 18:33:49 +0530

    "Footer Component"

--- Frontend/src/routes.tsx
+++ Frontend/src/routes.tsx
@@ -1,11 +1,12 @@
 import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/common/footer';
 
 const routes = (
     <>
     <Routes>
 
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Footer/>}/>
     </Routes>
     </>
 )

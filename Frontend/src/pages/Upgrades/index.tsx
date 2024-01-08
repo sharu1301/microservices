86edf91c5f4f779a227802b64ab3e7b82163186c
Ramya Mergu <ramya@insigniaconsultancy.com>
2024-01-08 12:43:43 +0530

    Upgrades page

---
+++ Frontend/src/pages/Upgrades/index.tsx
@@ -0,0 +1,228 @@
+import "./index.scss";
+import SubFooter from "../../components/subFooter";
+import PageTitle from "../../components/pageTitle";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Upgrades = () => {
+    return (
+        <>
+            <Header />
+            <PageTitle title="Upgrades" />
+            <div className="description-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-10 m-auto text-center">
+                            <h2 className="head">Upgrades</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur. Vitae sit ultrices vulputate tristique molestie non.
+                                Consectetur sit enim facilisi faucibus elementum feugiat. Iaculis urna vestibulum sed velit neque
+                                non eu. Id vitae ullamcorper praesent in ipsum. Leo pretium posuere vulputate feugiat non id.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section className="section processWrapper pt-0">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-5 col-lg-5">
+                            <div className="process-img">
+                                <img src="../../images/pages/upgrades/upgrades1.jpg" alt="upgrades" className="img-fluid" />
+                            </div>
+                        </div>
+                        <div className="col-md-7 col-lg-7">
+                            <div className="right-panel">
+                                <div className="panel bg-light">
+                                    <h6>Lorem ipsum dolor sit</h6>
+                                    <p>Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Praesent vitae venenatis. Maecenas
+                                        venenatis. Maecenas molestie sagittis. eros vel ante rutrum mollis in at mauris. Praesent,
+                                    </p>
+                                    <img src="../../images/pages/upgrades/upgrades1.jpg" alt="upgrades" className="img-fluid" />
+                                </div>
+                                <div className="panel-sub">
+                                    <h6>Lorem ipsum dolor sit amet consectet.</h6>
+                                </div>
+                                <div className="panel-sub">
+                                    <h6>Lorem ipsum dolor sit amet.</h6>
+                                </div>
+                                <div className="panel-sub">
+                                    <h6>Lorem ipsum dolor sit amet consectet.</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="introwrapper">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-7">
+                            <div className="introcontent pr-5">
+                                <h2>Lorem ipsum dolor sit</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Fusce varius faucibus massa sollicitudin amet augue.
+                                    Nibh metus a semper purus mauris duis. Lorem eu neque,
+                                    tristique quis duis. Nibh scelerisque ac adipiscing velit
+                                    non nulla in amet pellentesque.</p>
+                                <button>learn more</button>
+                            </div>
+                        </div>
+                        <div className="col-md-5">
+                            <img src="../../images/pages/upgrades/upgrades2.jpg" alt="upgrade" className="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section className="section servicesWrapper">
+                <div className="container">
+                    <div className="row box">
+                        <div className="col-md-6">
+                            <div className="service-img">
+                                <img
+                                    src="../../images/pages/upgrades/upgrades3.jpg"
+                                    alt="upgrade"
+                                    className="img-fluid"
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="service-content">
+                                <h2>Lorem ipsum dolor sit amet consectur </h2>
+                                <p className="pt-4">Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Praesent vitae venenatis. Maecenas
+                                venenatis. Maecenas molestie sagittis. eros vel ante rutrum mollis in at mauris. Praesent,
+                                pellen tesque hendrerit sapien suscipit. Fusce suscipit sed libero vitae venenatis. Maecenas
+                                venenatis. Maecenas molestie sagittis.</p>
+                                <p>venenatis. Maecenas molestie sagittis. eros vel ante rutrum mollis in at mauris. Praesent,
+                                pellen tesque hendrerit sapien suscipit. Fusce suscipit sed libero vitae venenatis. Maecenas
+                                </p>
+
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="mb-3">
+                                            <div className="form-check">
+                                                <label className="form-check-label">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        checked
+                                                    />
+                                                    Lorem ipsum amet consectur macnen
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="mb-3">
+                                            <div className="form-check">
+                                                <label className="form-check-label">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        checked
+                                                    />
+                                                    Lorem ipsum amet consectur macnen
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="mb-3">
+                                            <div className="form-check">
+                                                <label className="form-check-label">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        checked
+                                                    />
+                                                    Lorem ipsum amet consectur macnen
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="mb-3">
+                                            <div className="form-check">
+                                                <label className="form-check-label">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        checked
+                                                    />
+                                                    Lorem ipsum amet consectur macnen
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="customer-section">
+                <div className="container">
+                    <div className="row w-100">
+                        <div className="col-md-12 text-center mb-5">
+                            <h5>Our Customers</h5>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <div className="cust-wrapper">
+                                <div className="cust-img">
+                                    <img src="../../images/pages/upgrades/cust1.jpg" alt="customer" />
+                                </div>
+                                <span>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                </span>
+                                <p className="pt-4">I highly recommend XYZ Electronics to anyone looking for high-quality products 
+                                    and exceptional customer service. Thanks to Emily and the entire team for making 
+                                    my experience so great!"</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <div className="cust-wrapper">
+                                <div className="cust-img">
+                                    <img src="../../images/pages/upgrades/cust2.jpg" alt="customer" />
+                                </div>
+                                <span>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                </span>
+                                <p className="pt-4">I highly recommend XYZ Electronics to anyone looking for high-quality products 
+                                    and exceptional customer service. Thanks to Emily and the entire team for making 
+                                    my experience so great!"</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <div className="cust-wrapper">
+                                <div className="cust-img">
+                                    <img src="../../images/pages/upgrades/cust3.jpg" alt="customer" />
+                                </div>
+                                <span>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                    <i className="fa-solid fa-star ml-2"></i>
+                                </span>
+                                <p className="pt-4">I highly recommend XYZ Electronics to anyone looking for high-quality products 
+                                    and exceptional customer service. Thanks to Emily and the entire team for making 
+                                    my experience so great!"</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <SubFooter />
+            <Footer />
+        </>
+    )
+}
+
+export default Upgrades;

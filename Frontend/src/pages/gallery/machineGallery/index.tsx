f1908b9fd4bd69ce59155655369d7c4678a3feee
rajeshkumar <rajesh.kumar@insigniaconsultancy.com>
2024-01-15 19:16:49 +0530

    match padding as per Figma

--- Frontend/src/pages/gallery/machineGallery/index.tsx
+++ Frontend/src/pages/gallery/machineGallery/index.tsx
@@ -2,7 +2,7 @@ import "./index.scss";
 export default function MachineGallery(imageData: any) {
   return (
     <div className="continer">
-      <div className="grid-machine-container img-aspect">
+      <div className="grid-machine-container img-aspect px-8">
         {imageData?.imageData?.map((images, id) => (
           <div key={id}>
             <img src={images.url} alt="" />{" "}

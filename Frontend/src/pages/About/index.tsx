35edba71f6b660e8aa14cc659baa9ab101e3f888
Ramya Mergu <ramya@insigniaconsultancy.com>
2024-01-23 19:31:04 +0530

    about us content

--- Frontend/src/pages/About/index.tsx
+++ Frontend/src/pages/About/index.tsx
@@ -83,13 +83,13 @@ function About() {
                   </p>
                   <h6>Pioneering Innovation:</h6>
                   <p style={{ textAlign: 'justify' }}>
-                  In 1998, Parveen joined Joy Dzign Engineer, where he identified a burgeoning demand for high-quality plastic processing machines. In response, he founded Hinds Machineries in 1999, focusing on manufacturing machines with clamping forces ranging from 50 to 450 tons. The turning point came in 2003 with the launch of the Euro Series Injection Moulding Machine at PlastIndia, leading to rapid expansion. From a modest beginning, Hinds Plastic Machines evolved into a prominent player in the industry.
+                    In 1998, Parveen joined Joy Dzign Engineer, where he identified a burgeoning demand for high-quality plastic processing machines. In response, he founded Hinds Machineries in 1999, focusing on manufacturing machines with clamping forces ranging from 50 to 450 tons. The turning point came in 2003 with the launch of the Euro Series Injection Moulding Machine at PlastIndia, leading to rapid expansion. From a modest beginning, Hinds Plastic Machines evolved into a prominent player in the industry.
                   </p>
                   <h6>Milestones and Growth:</h6>
                   <p style={{ textAlign: 'justify' }}>
-                  2003 marked the launch of the Euro Series Injection Moulding Machine, propelling Hinds' expansion to a 450 sqm factory in Haryana IMT Manesar. Responding to escalating demand, another unit was established in Sector 8, IMT Manesar, leading to the rebranding as Hinds Plastic Machines Pvt Ltd. Today, our story is one of continuous innovation, engineering excellence, and a commitment to delivering state-of-the-art plastic processing solutions.
+                    2003 marked the launch of the Euro Series Injection Moulding Machine, propelling Hinds' expansion to a 450 sqm factory in Haryana IMT Manesar. Responding to escalating demand, another unit was established in Sector 8, IMT Manesar, leading to the rebranding as Hinds Plastic Machines Pvt Ltd. Today, our story is one of continuous innovation, engineering excellence, and a commitment to delivering state-of-the-art plastic processing solutions.
                   </p>
-                  
+
                 </div>
               </div>
             </div>
@@ -120,13 +120,11 @@ function About() {
 
           <section className="section aboutFeaturesWrapper">
             <div className="container">
-              <div className="row justify-content-center ml-0">
+              <div className="row justify-content-center ml-0 w-100">
                 <div className="col-md-10 col-12 col-lg-10 col-xl-7 text-center">
-                  <h3>Lorem ipsum dolor sit amet consectetur. Sed id id.</h3>
+                  <h3>Our Quality Commitment</h3>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse varius enim in eros elementum tristique. Duis
-                    cursus, mi quis viverra ornare.
+                    <b>Excellence Defined, Quality Assured, Support Guaranteed</b>
                   </p>
                 </div>
               </div>
@@ -136,11 +134,9 @@ function About() {
                     <div className="icon">
                       <img src="../../../images/pages/about-f1.svg" alt="icon" />
                     </div>
-                    <h4>ISO Certified Company</h4>
+                    <h4>ISO Certified Excellence</h4>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum
+                      Hinds Plastic Machines Pvt Ltd proudly holds ISO certification, showcasing our steadfast commitment to rigorous quality standards and operational excellence. This certification underscores our dedication to delivering products and services that surpass industry benchmarks, ensuring exceptional quality across all facets of our operations.
                     </p>
                   </div>
                 </div>
@@ -149,11 +145,9 @@ function About() {
                     <div className="icon">
                       <img src="../../../images/pages/about-f2.svg" alt="icon" />
                     </div>
-                    <h4>ISO Certified Company</h4>
+                    <h4>Top-Quality Assurance</h4>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum
+                      At Hinds Plastic Machines Pvt Ltd, top-notch quality is not just a goal; it's our standard. Our firm commitment to quality assurance permeates every aspect of our operations, from materials sourcing to manufacturing processes. We adhere to the highest standards, ensuring each product leaving our facilities is synonymous with durability, reliability, and uncompromising quality.
                     </p>
                   </div>
                 </div>
@@ -162,11 +156,9 @@ function About() {
                     <div className="icon">
                       <img src="../../../images/pages/about-f3.svg" alt="icon" />
                     </div>
-                    <h4>ISO Certified Company</h4>
+                    <h4>Dedicated Support</h4>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique. Duis
-                      cursus, mi quis viverra ornare, eros dolor interdum
+                    Experience exceptional support with Hinds Plastic Machines Pvt Ltd. Our commitment to support goes beyond product delivery. We stand by our clients, offering responsive and personalized assistance at every stage. Our team ensures your experience with our machines is seamless, backed by a reliable network of professionals ready to assist you.
                     </p>
                   </div>
                 </div>
@@ -180,13 +172,11 @@ function About() {
           <div className="row ml-0">
             <div className="col-md-6">
               <div className="left-content">
-                <div className="section_head">
-                  Lorem ipsum dolor sit amet consectetur. Sed id id.
+                <div className="section_head mb-3">
+                Precision Performance, Enhanced Production
                 </div>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse varius enim in eros elementum tristique. Duis
-                  cursus, mi quis viverra ornare, eros dolor
+                <b>Injection Moulding Machines: </b>Uncover precision engineering and robust performance with our Injection Moulding Machines. Hinds Plastic Machines Pvt Ltd proudly introduces state-of-the-art machines designed to meet diverse manufacturing needs. Engineered for efficiency and reliability, our machines elevate production processes, empowering your manufacturing capabilities.
                 </p>
               </div>
               <div className="responsive-img">
@@ -209,12 +199,7 @@ function About() {
                 />
               </div>
               <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse varius enim in eros elementum tristique. Duais
-                cursus, mi quis viverra ornare, eros dolor interdum Lorem
-                ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                varius enim in eros elementum tristique. Duis cursus, mi quis
-                viverra ornare, eros dolor interdum
+              <b>Auxiliary Equipments: </b>Maximize operational efficiency with our seamlessly integrated Auxiliary Equipments. At Hinds Plastic Machines Pvt Ltd, we understand the pivotal role of support in manufacturing. Our carefully curated range of Auxiliary Equipments offers comprehensive solutions for a streamlined production workflow. From material handling to temperature control, trust us to empower your manufacturing success.
               </p>
               <div className="responsive-img">
                 <img src="../../../images/pages/about-p1.png" className="img-fluid" alt="responsive" />
@@ -229,16 +214,16 @@ function About() {
 
       <section className="section companyInformation">
         <div className="container">
-          <div className="row justify-content-between company-head ml-0">
+          <div className="row justify-content-between company-head ml-0 w-100">
             <div className="col-md-6">
-              <h4> Lorem ipsum dolor sit amet consectetur. Sed id id.</h4>
+              <h4> Engineering Tomorrow's Plastic Revolution</h4>
             </div>
             <div className="col-md-5">
-              <p>
+              {/* <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Suspendisse varius enim in eros elementum tristique. Duis
                 cursus, mi quis viverra ornare, eros dolor
-              </p>
+              </p> */}
             </div>
           </div>
           <div className="row ptb-70 ml-0">
@@ -253,11 +238,9 @@ function About() {
                     />
                   </div>
                   <div className="company-content">
-                    <h6>Our Goals</h6>
+                    <h6>Why Hinds Plastic Machines</h6>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique.
-                      Duis cursus, mi quis viverra ornare, eros dolor
+                    Choose Hinds Plastic Machines for a journey that spans from the roots of engineering passion to the forefront of industry innovation. With a legacy marked by precision, dedication, and a deep understanding of manufacturing needs, we stand as the trusted partner for those seeking high-quality, efficient, and sustainable plastic processing solutions. Join us in shaping the future of manufacturing.
                     </p>
                   </div>
                 </div>
@@ -272,9 +255,7 @@ function About() {
                   <div className="company-content">
                     <h6>Our Mission</h6>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique.
-                      Duis cursus, mi quis viverra ornare, eros dolor
+                    Driven by a passion for innovation and engineering excellence, Hinds Plastic Machines is on a mission to revolutionize the plastic industry. Committed to delivering high-quality, efficient, and sustainable plastic processing solutions, our mission is to empower manufacturers with state-of-the-art machines that redefine industry standards.
                     </p>
                   </div>
                 </div>
@@ -289,9 +270,7 @@ function About() {
                   <div className="company-content">
                     <h6>Our Vision</h6>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Suspendisse varius enim in eros elementum tristique.
-                      Duis cursus, mi quis viverra ornare, eros dolor
+                    Hinds Plastic Machines envisions a future where precision and sustainability converge in the world of plastic processing. Our vision is to be a global leader, recognized for pioneering advancements in machine manufacturing. We aspire to shape a manufacturing landscape where efficiency, reliability, and environmental responsibility harmonize effortlessly.
                     </p>
                   </div>
                 </div>

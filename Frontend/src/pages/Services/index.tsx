cd79fea9427690bbc8c0fe5adf5dd193a276f9c4
Ramya Mergu <ramya@insigniaconsultancy.com>
2024-01-25 16:40:11 +0530

    images updated

--- Frontend/src/pages/Services/index.tsx
+++ Frontend/src/pages/Services/index.tsx
@@ -13,12 +13,12 @@ const Service = () => {
         <section className="section introWrapper">
           <div className="container">
             <div className="row justify-content-center">
-              <div className="col-md-8 text-center">
-                <h5>Hinds services</h5>
+              <div className="col-md-12 text-center">
+                <h5>Hinds customer service For a lifetime partnership</h5>
+              </div>
+              <div className="col-md-10 text-center">
                 <p className="pt-2">
-                  Unique and creative solutions that meet the clients’ expectations not only by 
-                  realizing the clients’ business objectives, but particularly by our strict 
-                  adherence to the ethical principles of public relations.
+                  The journey with HINDS extends far beyond the delivery of our injection molding machines or automation systems. Our commitment is to accompany you from the moment your equipment is set up in your facility through to its full operational lifespan. When the time comes to consider a new machine, we tailor our approach to meet your unique needs, ensuring a strategic investment that fosters your ongoing prosperity. At HINDS, exceptional customer service is at the heart of ensuring your total contentment.
                 </p>
               </div>
             </div>
@@ -50,81 +50,81 @@ const Service = () => {
               </div>
               <div className="col-md-6">
                 <div className="service-content">
-                <h2>Your Satisfaction is Our Goal! </h2>
-                <p className="pt-4">
-                Whether your Machine is two months or 02 years old, ensuring long term product satisfaction is our goal.
-                  {/* Lorem ipsum dolor sit amet, consec tetur adipi scing elit.
+                  <h2>Your Satisfaction is Our Goal! </h2>
+                  <p className="pt-4">
+                    Whether your Machine is two months or 02 years old, ensuring long term product satisfaction is our goal.
+                    {/* Lorem ipsum dolor sit amet, consec tetur adipi scing elit.
                   Praesent vitae venenatis. MaecenasLorem ipsum dolor sit amet,
                   consec tetur adipi scing elit. Praesent vitae venenatis.
                   MaecenasLorem ipsum dolor sit amet, consec tetur adipi scing
                   elit. Praesent vitae venenatis. Maecenas */}
-                </p>
-                <p>
-                Once your Injection Moulding Machine arrives, an experienced Hinds  service engineer will be at your door step to guide you for machine start-up & training. Our after sales service function with spare support service is staffed by Professional, Responsive & Customer Focused Personnel, offering dedicated support to Customer Network. It offers various Aftermarket Solutions & Support Activities for increasing your Profitability.
-                  {/* Lorem ipsum dolor sit amet, consec tetur adipi scing elit.
+                  </p>
+                  <p>
+                    Once your Injection Moulding Machine arrives, an experienced Hinds  service engineer will be at your door step to guide you for machine start-up & training. Our after sales service function with spare support service is staffed by Professional, Responsive & Customer Focused Personnel, offering dedicated support to Customer Network. It offers various Aftermarket Solutions & Support Activities for increasing your Profitability.
+                    {/* Lorem ipsum dolor sit amet, consec tetur adipi scing elit.
                   Praesent vitae venenatis. MaecenasLorem ipsum dolor sit amet,
                   consec tetur adipi scing elit. Praesent vitae venenatis.
                   Maecenas */}
-                </p>
+                  </p>
 
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <div className="form-check">
-                        <label className="form-check-label">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            checked
-                          />
-                          Lorem ipsum amet consectur macnen
-                        </label>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <div className="form-check">
+                          <label className="form-check-label">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              checked
+                            />
+                            Lorem ipsum amet consectur macnen
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <div className="form-check">
+                          <label className="form-check-label">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              checked
+                            />
+                            Lorem ipsum amet consectur macnen
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <div className="form-check">
+                          <label className="form-check-label">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              checked
+                            />
+                            Lorem ipsum amet consectur macnen
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <div className="form-check">
+                          <label className="form-check-label">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              checked
+                            />
+                            Lorem ipsum amet consectur macnen
+                          </label>
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <div className="form-check">
-                        <label className="form-check-label">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            checked
-                          />
-                          Lorem ipsum amet consectur macnen
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <div className="form-check">
-                        <label className="form-check-label">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            checked
-                          />
-                          Lorem ipsum amet consectur macnen
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <div className="form-check">
-                        <label className="form-check-label">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            checked
-                          />
-                          Lorem ipsum amet consectur macnen
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 </div>
               </div>
             </div>
@@ -135,46 +135,42 @@ const Service = () => {
           <div className="container">
             <div className="row justify-content-between">
               <div className="col-md-5">
-                <h4>Lorem ipsum dolor sit</h4>
+                <h4>Always there where you need us</h4>
               </div>
               <div className="col-md-7">
-                <p>Lorem ipsum dolor sit amet consectetur. Vitae sit ultrices
-                vulputate tristique molestie non. Consectetur sit enim facilisi
-                faucibus elementum feugiat. Iaculis urna vestibulum sed velit
-                neque non eu. Id vitae ullamcorper praesent in ipsum. Leo
-                pretium posuere vulputate feugiat non id.</p>
+                <p>Operational delays can be expensive, which is why you rely on HINDS customer service for clear communication, prompt support, and immediate hotline assistance, among other services. We deliver exactly that—responsive, on-demand plastic injection molding services to meet your needs without delay.</p>
               </div>
             </div>
             <div className="row mt-65 w-100">
               <div className="col-md-3 col-6">
                 <div className="counterWrapper">
-                  <div className="counterValue">100+</div>
+                  {/* <div className="counterValue">24/7</div> */}
                   <div className="counterLabel">
-                    <p>Lorem ipsum dolor sit</p>
+                    <p>24/7 availability</p>
                   </div>
                 </div>
               </div>
               <div className="col-md-3 col-6">
                 <div className="counterWrapper">
-                  <div className="counterValue">2K+</div>
+                  {/* <div className="counterValue">2K+</div> */}
                   <div className="counterLabel">
-                    <p>Lorem ipsum dolor sit</p>
+                    <p>720 speedy assistance on-site and online support</p>
                   </div>
                 </div>
               </div>
               <div className="col-md-3 col-6">
                 <div className="counterWrapper">
-                  <div className="counterValue">10+</div>
+                  {/* <div className="counterValue">10+</div> */}
                   <div className="counterLabel">
-                    <p>Lorem ipsum dolor sit</p>
+                    <p>Immediate delivery of your spare parts</p>
                   </div>
                 </div>
               </div>
               <div className="col-md-3 col-6">
                 <div className="counterWrapper">
-                  <div className="counterValue">15</div>
+                  {/* <div className="counterValue">15</div> */}
                   <div className="counterLabel">
-                    <p>Lorem ipsum dolor sit</p>
+                    <p>Support from our in-house injection moulding experts</p>
                   </div>
                 </div>
               </div>
@@ -193,7 +189,7 @@ const Service = () => {
               <div className="col-md-6">
                 <h2>Retrofit Solutions </h2>
                 <p className="pt-4">
-                We, Hinds Plastic Machines Pvt Ltd., provide controlled Retrofitment solutions for our machines. Our service can increase the productivity level throughout the life of your machines.
+                  We, Hinds Plastic Machines Pvt Ltd., provide controlled Retrofitment solutions for our machines. Our service can increase the productivity level throughout the life of your machines.
                 </p>
                 <p>
                   Lorem ipsum dolor sit amet, consec tetur adipi scing elit.

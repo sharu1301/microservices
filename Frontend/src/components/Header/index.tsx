deeab45a91751170a0a1122aae9b4914b2f30a4c
nikhilkaimalinsignia <130687289+nikhilkaimalinsignia@users.noreply.github.com>
2024-01-30 18:33:00 +0530

    bug fixes

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -6,14 +6,14 @@ import linkedIn from "../../assets/icons/linkedin.png";
 import instagram from "../../assets/icons/instagram.png";
 import logo from "../../assets/logo/logo.png";
 import { useState } from "react";
-import productData from '../../data/products.json';
-
+import productData from "../../data/products.json";
 
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const [openMenu, setOpenMenu] = useState(false);
-  const pathname = location.pathname
+  const pathname = location.pathname;
+
   return (
     <>
       <div className="headerContainer">
@@ -55,7 +55,11 @@ export default function Header() {
                     </a>
                   </div>
                   <div className={"socialIcons03"}>
-                    <a href="https://www.linkedin.com/in/parveen-sharma-02678a131/?originalSubdomain=in" target="_blank" rel="noreferrer">
+                    <a
+                      href="https://www.linkedin.com/in/parveen-sharma-02678a131/?originalSubdomain=in"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       <img
                         className={"e3ac851901b7444af8c2c6XLogoIcon1"}
                         alt=""
@@ -68,39 +72,87 @@ export default function Header() {
             </div>
           </div>
         </div>
-
         <div className="navbar">
           <div className="container">
-            <img src={logo} className="logoIcon" alt="" onClick={() => navigate("/")} />
-
-            <div className={openMenu ? 'navList active' : 'navList'} >
+            <img
+              src={logo}
+              className="logoIcon"
+              alt=""
+              onClick={() => navigate("/")}
+            />
+            <div className={openMenu ? "navList active" : "navList"}>
               <ul>
-                <li className={pathname === "/" ? 'active' : ''} onClick={() => navigate("/")}>Home</li>
-                <li className={pathname === "/about" ? 'active' : ''} onClick={() => navigate("/about")}>About Us</li>
-
-
-                <li className="dropdown m-0 p-0">
-                  <li className={pathname === "/service" ? 'active dropdown-toggle' : 'dropdown-toggle'}
-                    data-bs-toggle="dropdown" onClick={() => navigate("/service")}>Services</li>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="/service">About Services</a></li>
-                    <li><a className="dropdown-item" href="/process">Process Optimization</a>
-                    </li>
-                    <li><a className="dropdown-item" href="/upgrades">Upgrades</a>
-                    </li>
-                    <li><a className="dropdown-item" href="/customermaintenance">Customer Service and Maintenance</a></li>
-                  </ul>
+                <li
+                  className={pathname === "/" ? "active" : ""}
+                  onClick={() => navigate("/")}
+                >
+                  Home
+                </li>
+                <li
+                  className={pathname === "/about" ? "active" : ""}
+                  onClick={() => navigate("/about")}
+                >
+                  About Us
                 </li>
 
-                <li className="dropdown">
-                  <li className="dropdown-toggle m-0 p-0" data-bs-toggle="dropdown">Products</li>
+                <li className="dropdown m-0 p-0">
+                  <li
+                    className={
+                      pathname === "/service"
+                        ? "active dropdown-toggle"
+                        : "dropdown-toggle"
+                    }
+                    data-bs-toggle="dropdown"
+                    onClick={() => navigate("/service")}
+                  >
+                    Services
+                  </li>
                   <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="/productlist">Injection moulding machine</a>
+                    <li>
+                      <a className="dropdown-item" href="/service">
+                        About Services
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/process">
+                        Process Optimization
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/upgrades">
+                        Upgrades
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="/customermaintenance">
+                        Customer Service and Maintenance
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="dropdown">
+                  <li
+                    className="dropdown-toggle m-0 p-0"
+                    data-bs-toggle="dropdown"
+                  >
+                    Products
+                  </li>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="/productlist">
+                        Injection moulding machine
+                      </a>
                       <ul className="submenu dropdown-menu">
                         {productData.map((list, i) => (
-                          <li key={i}><a className="dropdown-item" href={`/product-specification/${list.title}`}>{list.title}</a></li>
+                          <li key={i}>
+                            <a
+                              className="dropdown-item"
+                              href={`/product-specification/${list.title}`}
+                            >
+                              {list.title}
+                            </a>
+                          </li>
                         ))}
-
                       </ul>
                     </li>
                     {/* <li><a className="dropdown-item" href="/productlist">Blow moulding machine</a>
@@ -112,36 +164,42 @@ export default function Header() {
                         ))}
                       </ul>
                     </li> */}
-                    <li><a className="dropdown-item" href="/auxiliaries">Auxiliary Parts</a></li>
+                    <li>
+                      <a className="dropdown-item" href="/auxiliaries">
+                        Auxiliary Parts
+                      </a>
+                    </li>
                   </ul>
                 </li>
+                <li
+                  className={pathname === "/applications" ? "active" : ""}
+                  onClick={() => navigate("/applications")}
+                >
+                  Applications
+                </li>
+                <li
+                  className={pathname === "/gallery" ? "active" : ""}
+                  onClick={() => navigate("/gallery")}
+                >
+                  Gallery
+                </li>
 
-
-
-                <li className={pathname === "/applications" ? 'active' : ''} onClick={() => navigate("/applications")} >Applications</li>
-                <li className={pathname === "/gallery" ? 'active' : ''} onClick={() => navigate("/gallery")}>Gallery</li>
-
-
-
-                <li className={pathname === "/career" ? 'active' : ''} onClick={() => navigate("/career")}>Career</li>
+                <li
+                  className={pathname === "/career" ? "active" : ""}
+                  onClick={() => navigate("/career")}
+                >
+                  Career
+                </li>
               </ul>
-
               <div
                 className="contactUsParent"
                 onClick={() => navigate("/contactus")}
               >
                 <b className="contactUs"> Contact Us</b>
               </div>
-
             </div>
-
-
-            <div id="mobile"
-              onClick={() => setOpenMenu(!openMenu)}
-            >
-              <i className={
-                openMenu ? 'fas fa-times' : 'fas fa-bars'
-              }> </i>
+            <div id="mobile" onClick={() => setOpenMenu(!openMenu)}>
+              <i className={openMenu ? "fas fa-times" : "fas fa-bars"}> </i>
             </div>
           </div>
         </div>

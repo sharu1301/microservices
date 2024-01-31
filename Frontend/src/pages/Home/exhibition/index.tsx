426d4146c2df0e0a4984a015a611685b7daff197
Ramya Mergu <ramya@insigniaconsultancy.com>
2024-01-31 16:42:42 +0530

    minor fixes

--- Frontend/src/pages/Home/exhibition/index.tsx
+++ Frontend/src/pages/Home/exhibition/index.tsx
@@ -56,6 +56,7 @@ export default function Exhibition() {
     return (
         <>
             <div className='container'>
+                <div className="row w-100 ml-0">
                 <h2 className='heading pt-5'> Around The Exhibition</h2>
                 <p className='description'>Join us at the upcoming industry exhibition to explore the latest innovations and advancements in injection molding technology. Visit the Hinds Machines booth to experience firsthand our state-of-the-art machinery, tailored solutions, and expert insights that can elevate your manufacturing processes.</p>
 
@@ -73,6 +74,7 @@ export default function Exhibition() {
 
 
                     <ArrowRight />
+                </div>
                 </div>
             </div>
         </>

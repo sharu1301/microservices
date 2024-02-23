679271d0d41ed071e460a75f844764432423528e
sonal <sonal@insigniasonsultancy.com>
2024-02-23 21:05:21 +0530

    changes

--- Frontend/src/pages/ProductSpecification/index.tsx
+++ Frontend/src/pages/ProductSpecification/index.tsx
@@ -68,121 +68,116 @@ export default function ProductSpecification() {
     <>
       <Header />
       <PageTitle title={productname} subtitle={productname} />
-      <ImageMapperComponent
+      {/* <ImageMapperComponent
         src={mainImg}
         areas={[{ name: "unit 1", shape: "rect", coords: [40, 40, 60, 60] }]}
-      />
-      <SectionModal />
+      /> */}
+
       <div>
         {injectionData
           ? injectionMouldingData
-              ?.filter(
-                (data) =>
-                  data.title.toLowerCase() === productname?.toLowerCase()
-              )
-              .map((data: any, index) => (
-                <div key={index}>
-                  <div className="mainSectionContainer row">
-                    <div className="imgSlider col-md-7">
-                      <Slider {...MainSliderSettings}>
-                        {data?.mainImg?.map((image, index) => (
-                          <img
-                            key={index}
-                            src={require(`../../assets/${image}`)}
-                            className="image"
-                            alt="lorem"
-                            style={{ cursor: "pointer" }}
-                          />
+            ?.filter(
+              (data) =>
+                data.title.toLowerCase() === productname?.toLowerCase()
+            )
+            .map((data: any, index) => (
+              <div key={index}>
+                <div className="mainSectionContainer row">
+                  <div className="imgSlider col-md-7">
+                    <ImageMapperComponent
+                      src={data?.mainImg}
+                      areas={[{ name: "unit 1", shape: "rect", coords: [40, 40, 60, 60] }]}
+                    />
+                    <SectionModal image={data?.mainImg} />
+
+                  </div>
+                  <div className="detailcard col-md-6">
+                    <div>
+                      <b> End Application</b>
+                      <ul className="pl-4">
+                        {data?.industry?.map((industries, i) => (
+                          <li key={i}>{industries}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="slider">
+                      <Slider {...settings}>
+                        {data?.sliderImages?.map((images, i) => (
+                          <div key={i}>
+                            <img
+                              src={require(`../../assets/${images}`)}
+                              alt="lorem"
+                            />
+                          </div>
                         ))}
                       </Slider>
                     </div>
-                    <div className="detailcard col-md-6">
-                      <div>
-                        <b> End Application</b>
-                        <ul className="pl-4">
-                          {data?.industry?.map((industries, i) => (
-                            <li key={i}>{industries}</li>
-                          ))}
-                        </ul>
-                      </div>
-                      <div className="slider">
-                        <Slider {...settings}>
-                          {data?.sliderImages?.map((images, i) => (
-                            <div key={i}>
-                              <img
-                                src={require(`../../assets/${images}`)}
-                                alt="lorem"
-                              />
-                            </div>
-                          ))}
-                        </Slider>
-                      </div>
-                      <div className="mt-5 mb-4">
-                        <b>Standard Features</b>
-                        <ul className="pl-4">
-                          {data?.standardFeatures?.map((features, index) => (
-                            <li key={index}>{features}</li>
-                          ))}
-                        </ul>
-                      </div>
-                      <div
-                        className={"btnContainer mt-2"}
-                        onClick={() => navigate("/productenquiry")}
-                      >
-                        <img src={downArrow} alt="" />
-                        <b>Download Broucher</b>
-                      </div>
+                    <div className="mt-5 mb-4">
+                      <b>Standard Features</b>
+                      <ul className="pl-4">
+                        {data?.standardFeatures?.map((features, index) => (
+                          <li key={index}>{features}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div
+                      className={"btnContainer mt-2"}
+                      onClick={() => navigate("/productenquiry")}
+                    >
+                      <img src={downArrow} alt="" />
+                      <b>Download Broucher</b>
                     </div>
                   </div>
-                  <div className="description">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-md-6">
-                          <div className="leftSection">
-                            <b className="title">{data.description}</b>
-                            {/* <b className='subTitle'>Clamping Force 90-350</b> */}
-                          </div>
+                </div>
+                <div className="description">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div className="leftSection">
+                          <b className="title">{data.description}</b>
+                          {/* <b className='subTitle'>Clamping Force 90-350</b> */}
                         </div>
-                        <div className="col-md-6">
-                          <div className="rightSection">
-                            <b className="title">
-                              Hinds Machines, a leading name in the field of
-                              machine manufacturerers, is dedicated to providing
-                              cutting-edge solutions for diverse industrial
-                              needs.
-                            </b>
-                            <p className="subTitle">
-                              With a rich history spanning decades, their
-                              commitment to innovation and quality has set them
-                              apart in the industry. Renowned for their
-                              precision engineering and advanced technology,
-                              Hinds Machines consistently delivers reliable,
-                              high-performance products that meet the demands of
-                              modern manufacturing. Backed by a team of seasoned
-                              experts, Hinds Machines remains at the forefront
-                              of the injection molding machinery sector, setting
-                              the standard for excellence and customer
-                              satisfaction.
-                            </p>
-                          </div>
+                      </div>
+                      <div className="col-md-6">
+                        <div className="rightSection">
+                          <b className="title">
+                            Hinds Machines, a leading name in the field of
+                            machine manufacturerers, is dedicated to providing
+                            cutting-edge solutions for diverse industrial
+                            needs.
+                          </b>
+                          <p className="subTitle">
+                            With a rich history spanning decades, their
+                            commitment to innovation and quality has set them
+                            apart in the industry. Renowned for their
+                            precision engineering and advanced technology,
+                            Hinds Machines consistently delivers reliable,
+                            high-performance products that meet the demands of
+                            modern manufacturing. Backed by a team of seasoned
+                            experts, Hinds Machines remains at the forefront
+                            of the injection molding machinery sector, setting
+                            the standard for excellence and customer
+                            satisfaction.
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="type">
-                    <div className="card1">
-                      <b className="title">{data?.Type}</b>
-                      <p className="description">Type</p>
-                    </div>
-                    <div className="card1">
-                      <b className="title">{data?.DriveType}</b>
-                      <p className="description">Drive Type</p>
-                    </div>
-                    <div className="card1">
-                      <b className="title">{data?.ClampingForce}</b>
-                      <p className="description">Clamping Force</p>
-                    </div>
-                    {/* <div className='card1'>
+                </div>
+                <div className="type">
+                  <div className="card1">
+                    <b className="title">{data?.Type}</b>
+                    <p className="description">Type</p>
+                  </div>
+                  <div className="card1">
+                    <b className="title">{data?.DriveType}</b>
+                    <p className="description">Drive Type</p>
+                  </div>
+                  <div className="card1">
+                    <b className="title">{data?.ClampingForce}</b>
+                    <p className="description">Clamping Force</p>
+                  </div>
+                  {/* <div className='card1'>
                     {data?.cards?.map((cardData, i) =>
                       (<b className='title' key={i}>{cardData?.DriveType}</b>))}
                     <p className='description'>Drive Type</p>
@@ -192,49 +187,49 @@ export default function ProductSpecification() {
                       (<b className='title' key={i}>{cardData?.ClampingForce}</b>))}
                     <p className='description'>Clamping Force</p>
                   </div> */}
+                </div>
+                <div className="bottomSlider">
+                  <div className="image-section">
+                    <img
+                      src={
+                        footerImg ||
+                        require(`../../assets/${data.overviewImages[0]}`)
+                      }
+                      alt=""
+                    />
                   </div>
-                  <div className="bottomSlider">
-                    <div className="image-section">
-                      <img
-                        src={
-                          footerImg ||
-                          require(`../../assets/${data.overviewImages[0]}`)
+                  <div className={"smallContainer"}>
+                    {data.overviewImages?.map((image, index) => (
+                      <div
+                        key={index}
+                        onClick={() =>
+                          setFooterImg(require(`../../assets/${image}`))
                         }
-                        alt=""
-                      />
-                    </div>
-                    <div className={"smallContainer"}>
-                      {data.overviewImages?.map((image, index) => (
-                        <div
-                          key={index}
-                          onClick={() =>
-                            setFooterImg(require(`../../assets/${image}`))
-                          }
-                        >
-                          <img
-                            className={footerImg === image ? "active" : "image"}
-                            src={require(`../../assets/${image}`)}
-                            alt=""
-                          />
-                        </div>
-                      ))}
-                    </div>
-                    <div className="paraContainer">
-                      {/* <p>Click to Zoom</p> */}
-                    </div>
+                      >
+                        <img
+                          className={footerImg === image ? "active" : "image"}
+                          src={require(`../../assets/${image}`)}
+                          alt=""
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="paraContainer">
+                    {/* <p>Click to Zoom</p> */}
                   </div>
                 </div>
-              ))
-          : blowMoulding && (
-              <div className="reactplayer">
-                <ReactPlayer
-                  url={blowMoulding?.videoLink}
-                  loop={true}
-                  controls={true}
-                />
-                {/* )} */}
               </div>
-            )}
+            ))
+          : blowMoulding && (
+            <div className="reactplayer">
+              <ReactPlayer
+                url={blowMoulding?.videoLink}
+                loop={true}
+                controls={true}
+              />
+              {/* )} */}
+            </div>
+          )}
         <SubFooter />
         <Footer />
       </div>

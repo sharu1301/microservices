624f4a069f31843e7cbfe6d6c2c6838799984dd8
nikhilkaimalinsignia <130687289+nikhilkaimalinsignia@users.noreply.github.com>
2024-02-27 14:02:27 +0530

    code alignment

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,15 @@
-import './App.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
-import routes from './routes';
-import "../src/assets/sass/main.scss"
-
+import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import routes from "./routes";
+import "../src/assets/sass/main.scss";
 
 function App() {
-
-
   return (
     <>
       {/* <Header/> */}
 
-      <Router>
-        {routes}
-      </Router>
+      <Router>{routes}</Router>
     </>
-
-
   );
 }
 
